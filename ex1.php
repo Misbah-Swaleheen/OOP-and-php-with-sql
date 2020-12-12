5251d4f24d0b4f929f8c8f52e426f58fe7f2e0b5
Misbah-Swaleheen <54136143+Misbah-Swaleheen@users.noreply.github.com>
2020-12-12 17:01:49 +0500

    Add files via upload

--- ex1.php
+++ ex1.php
@@ -22,7 +22,7 @@
     }
 
 
-class rat{
+class mouse{
     public $eyes;
     public $ears;
     public $furs;
@@ -41,4 +41,83 @@ class rat{
 
     function biting(){}
 }
+
+class book{
+    public $pages;
+    public $title;
+    public $color;
+    public $price;
+
+    function reading(){}
+
+    function writing(){}
+
+    function purchasing(){}
+
+    function selling(){}
+
+}
+
+class bicycle{
+    public $wheels;
+    public $horn;
+    public $color;
+    public $chain;
+    public $gear;
+    public $pedals;
+
+    function starting(){}
+
+    function riding(){}
+
+    function breaks(){}
+
+    function accelerating(){}
+}
+
+class person{
+    public $tallShort;
+    public $thinThick;
+    public $weight;
+    public $height;
+    public $eyes;
+    public $nose;
+    public $legs;
+    public $ears;
+    public $skintone;
+    public $eyeColor;
+    public $hairColor;
+    public $habbits=[];
+
+    function eating(){}
+
+    function drinking(){}
+
+    function thinking(){}
+
+    function cooking(){}
+
+    function professtion(){}
+
+    function sleeping(){}
+
+    function walking(){}
+
+    function running(){}
+
+    function behaviour(){}
+}
+
+class computerScience{
+    public $languages;
+    public $fields;
+    
+
+
+    function makingSoftware(){}
+
+
+}
+
+
 ?>
---
+++ withWhile.php
@@ -0,0 +1,65 @@
+<?php
+    // Model Class which has only attributes not methods
+    class student{
+        public $id;
+        public $name;
+        public $email;
+        private $password;
+
+    }
+
+    $s1 = new student();
+    $s1 -> id = 1;
+    $s1 -> name = "ali"; 
+    $s1 -> email =  $s1 -> name . '@gmail.com';
+
+    $s2 = new student();
+    $s2 -> id = 2;
+    $s2 -> name = "Faizan"; 
+    $s2 -> email =  $s2 -> name . '@gmail.com';
+
+    $students = [$s1, $s2]
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $i =0;
+                while( $i < count($students)){
+                    $items = $students[$i];
+            ?>
+
+            <tr>
+                    <td><?php echo $items -> id ?></td>
+                    <td><?php echo $items -> name ?></td>
+                    <td><?php echo $items -> email ?></td>
+            </tr>
+
+        <?php
+            $i++;
+                }
+                ?>
+        </tbody>
+    </table>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+</html>
