8d9137884db9975253fda941f37c01dcde210fba
Misbah-Swaleheen <54136143+Misbah-Swaleheen@users.noreply.github.com>
2020-12-17 13:42:58 +0500

    Add files via upload

---
+++ o2.php
@@ -0,0 +1,66 @@
+<?php
+    class calculator{
+        public function __call($name, $arguments)
+        {
+            if($name == 'add'){
+                switch(count($arguments)){
+                    case 2:
+                        return $arguments[0] + $arguments[1];
+                        break;
+                    case 3:
+                        return $arguments[0] + $arguments[1] + $arguments[2];
+                        break;
+                    default:
+                        return "not match";
+                        break;
+                }
+            }
+            elseif ($name == 'sub') {
+                # code...
+                switch(count($arguments)){
+                    case 2:
+                        return $arguments[0] - $arguments[1];
+                        break;
+                   
+                    default:
+                        return "not match";
+                        break;
+                }
+            }
+            elseif ($name == 'mul') {
+                # code...
+                switch(count($arguments)){
+                    case 2:
+                        return $arguments[0] * $arguments[1];
+                        break;
+                    case 3:
+                        return $arguments[0] * $arguments[1]  * $arguments[2];
+                        break;
+                    default:
+                        return "not match";
+                        break;
+                }
+            }
+            elseif ($name == 'div') {
+                # code...
+                switch(count($arguments)){
+                    case 2:
+                        return $arguments[0] / $arguments[1];
+                        break;
+                    
+                    default:
+                        return "not match";
+                        break;
+                }
+            }
+            else{
+                return "not match";
+            }
+        }
+     
+    }
+
+    $obj = new calculator();
+    echo $obj->add(6,3,8);
+
+?>
